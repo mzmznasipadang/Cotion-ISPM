cd457f22dacaacbbb9f8cb9ae6127f1699f2daa8
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2023-05-26 13:13:58 +0700

    The creation of Login

---
+++ validate_login.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // redirect to home page or dashboard
+    header('Location: home.html');
+} else {
+    echo "Invalid username or password";
+}
+
+$conn->close();
+?>
